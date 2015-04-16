591763294ed9a12c135115f0403e9a796ba67854
Ethan Radd <ethanradd@yahoo.com>
2015-04-16 16:02:57 +0800

    Added changes to local/database

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'host'      => getenv('db_host'),
+			'database'  => 'authapp',
+			'username'  => getenv('db_username'),
+			'password'  => getenv('db_password'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
